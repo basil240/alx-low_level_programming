3860dbfba1fcdad565a5e54ddbbd7dd42aaa0bb3
EKPEI IKECHUKWU <iekpei@yahoo.com>
2023-01-09 23:58:38 -0800

    argue

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,7 +1,9 @@
-#include <stdio.h>                                                             i#include "main.h"                                                               /**                                                                     
+#include <stdio.h>   
+#include "main.h" 
+/**                                                                     
  * main - print the name of the program                                          * @argc: Count arguments                                                        * @argv: Arguments            
  *                                                                               * Return: Always 0 (Success)                                          
  */                                                                             int main(int argc, char *argv[]) 
 {
-	(void) argv; /*Ignore argv*/                                                    printf("%i\n", argc - 1);                                                       return (0);                                                            
+	(void) argv; /*Ignore argv*/                                                    printf("%i\n", argc - 1);                                                       return (0);           
 }
